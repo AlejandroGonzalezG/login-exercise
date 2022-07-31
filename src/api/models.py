d2ec827d5dc7d5e48202bb99ec5988b45031dec7
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-07-31 04:52:01 +0000

    avance login exercise api

--- src/api/models.py
+++ src/api/models.py
@@ -3,17 +3,58 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
+    profile = db.relationship('Profile', backref='user', uselist=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "profile":self.profile.serialize()
             # do not serialize the password, its a security breach
         }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Profile(db.Model):
+    __tablename__='profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), default="")
+    instagram = db.Column(db.String(50), default="")
+    facebook = db.Column(db.String(50), default="")
+    twitter = db.Column(db.String(50), default="")
+    github = db.Column(db.String(50), default="")
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "instagram":self.instagram,
+            "facebook":self.facebook,
+            "twitter":self.twitter,
+            "github":self.github,
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
