4b81d51bed62842c48ee7c5506862b682785f54f
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-08-01 01:40:07 +0000

    avance front

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,15 +3,34 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					MyBlog.com
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav">
+						<a className="nav-link" href="#">
+							Signup
+						</a>
+						<a className="nav-link" href="#">
+							Login
+						</a>
+						<a className="nav-link" href="#">
+							My Profile
+						</a>
+					</div>
 				</div>
 			</div>
 		</nav>
