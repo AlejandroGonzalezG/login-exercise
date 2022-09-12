2d8cd5dd0a774a91aeb0c5e34c4326d0f731e031
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-09-12 18:04:37 -0300

    fixes basedatos e implementación RegEx a register

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    is_active = db.Column(db.Boolean(), default=True)
+    is_active = db.Column(db.Boolean(), default=True, nullable=False)
     profile = db.relationship('Profile', backref='users', uselist=False)
 
     def serialize(self):
@@ -32,13 +32,13 @@ class User(db.Model):
 class Profile(db.Model):
     __tablename__ = 'profiles'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), default="")
-    phone_number = db.Column(db.String(20))
+    name = db.Column(db.String(100), default="Mi nombre")
+    phone_number = db.Column(db.String(20), default="+569")
     instagram = db.Column(db.String(50), default="@something")
     facebook = db.Column(db.String(50), default="facebook.com/something")
     twitter = db.Column(db.String(50), default="twitter.com/something")
     github = db.Column(db.String(50), default="github.com/Something")
-    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def serialize(self):
         return {
--- src/front/js/pages/Signup.js
+++ src/front/js/pages/Signup.js
@@ -1,6 +1,8 @@
 import { useHistory } from "react-router-dom";
 import { Context } from "../store/appContext";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import PasswordRequisito from './PasswordRequisito';
+import '../../styles/index.css'
 
 
 const estiloBoton = {
@@ -11,13 +13,40 @@ const estiloBoton = {
 const Signup = () => {
     const { store, actions } = useContext(Context);
     const history = useHistory();
+    const [passRequisito, setPassRequisito] = useState(false);
+    const [revisar, setRevisar] = useState({
+        checkMayus: false,
+        checkNumero: false,
+        checkLargo: false,
+        checkCaracterEspecial: false,
+    })
+    const handleOnFocus = () => {
+        setPassRequisito(true);
+    }
+
+    const handleOnBlur = () => {
+        setPassRequisito(false);
+    }
+
+    const checkValidation = () => {
+        const checkMayus = /[A-Z]/.test(store.password);
+        const checkNumero = /[0-9]/.test(store.password);
+        const checkLargo = store.password.length >= 8;
+        const checkCaracterEspecial = /[!@#$%&?¡*°]/.test(store.password);
+        setRevisar({
+            checkMayus,
+            checkNumero,
+            checkLargo,
+            checkCaracterEspecial
+        })
+    };
 
     useEffect(() => {
 
     }, [])
 
     useEffect(() => {
-        if (store.currentUser !== null) history.push('/');
+        if (store.currentUser === null) history.push('/register');
     }, [store])
 
     return (
@@ -26,7 +55,15 @@ const Signup = () => {
                 <h1>Register</h1>
                 <form className="formulario m-5" onSubmit={(e) => actions.handleRegister(e, history)}>
                     <input type="text" className="form-control" id="email" name="email" value={store.email} onChange={actions.handleChange} placeholder="Username" style={estiloBoton} />
-                    <input placeholder="Password" type="password" className="form-control" id="password" name="password" value={store.password} onChange={actions.handleChange} style={estiloBoton} />
+                    <input placeholder="Password" type="password" className="form-control" id="password" name="password" value={store.password} onChange={actions.handleChange} onFocus={handleOnFocus}
+                        onBlur={handleOnBlur} onKeyUp={() => checkValidation()} style={estiloBoton} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&?¡*°]).{8,}" />
+                    {passRequisito ? (
+                        <PasswordRequisito
+                            checkMayus={revisar.checkMayus ? "valid" : "invalid"}
+                            checkNumero={revisar.checkNumero ? "valid" : "invalid"}
+                            checkLargo={revisar.checkLargo ? "valid" : "invalid"}
+                            checkCaracterEspecial={revisar.checkCaracterEspecial ? "valid" : "invalid"} />
+                    ) : null}
                     <button type="submit" className="btn btn-primary btn-block btn-large" style={estiloBoton}>
                         Registrarte
                     </button>
